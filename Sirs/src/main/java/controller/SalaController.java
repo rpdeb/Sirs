3a5582908f37636eb156afaa90535efa487fc684
Debora Ribeiro Pereira <52083218+rpdeb@users.noreply.github.com>
2022-05-31 11:38:01 -0300

    Commit Inicial

---
+++ Sirs/src/main/java/controller/SalaController.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class SalaController {
+
+	public SalaController() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Sirs/src/main/java/model/Sala.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Sala {
+
+	public Sala() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
